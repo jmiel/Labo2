7f2a32c224d066065126b1cc26e730c823eb3c19
jmiel <jonathan.miel@outlook.com>
2018-02-15 09:56:21 +0100

    do (c)

--- Exo1.py
+++ Exo1.py
@@ -6,10 +6,16 @@ p=re.compile(pattern)
 
 print(p.match(phonenumber))'''
 
-number=input ('Write a positive or negative number without any spaces : \n')
+'''number=input ('Write a positive or negative number without any spaces : \n')
 pattern1=r'-?[1-9][0-9]*'
 #pattern2=r'\-\d*'
 p1=re.compile(pattern1)
 #p2=re.compite(pattern2)
 
-print(p1.match(number))
+print(p1.match(number))'''
+
+'''immatriculation=input('Write a licence plate with the pattern XLLLDDD or XDDDLLL : \n')
+pattern=r'[1-9][A-Z]{3}\d{3}|[1-9]\d{3}[A-Z]{3}'
+p=re.compile(pattern)
+
+print(p.match(immatriculation))'''
